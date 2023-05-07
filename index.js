3848ffe16dc24e69f76ca66ac6762a2fc005a439
Catherine Muya <catemuya77@gmail.com>
2023-05-07 14:06:52 +0200

    "add Toggle Functionality"

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// phew… not a lot going on here. Please add some code!
+// Select all Bookmarks (Below) //
+// const toggleBookmarks = document.querySelectorAll(".bookmark");
+
+// toggleBookmarks.forEach((bookmark) => {
+//   function toggle() {
+//     bookmark.classList.toggle("bookmark--active");
+//   }
+
+//   bookmark.addEventListener("click", toggle);
+// });
+
+//-----> Select the first Bookmark <------//
+
+const toggleBookmarkButton = document.querySelector(".bookmark");
+
+function toggleBookmark() {
+  toggleBookmarkButton.classList.toggle("bookmark--active");
+}
+
+toggleBookmarkButton.addEventListener("click", toggleBookmark);
+
+//--->        ------>
+
+const toggleAnswerButton = document.querySelector(".card__button-answer");
+const answer = document.querySelector(".card__answer");
+
+function toggleAnswer() {
+  const wasToggled = answer.classList.toggle("card__answer--active");
+  toggleAnswerButton.textContent = wasToggled ? "Hide answer" : "Show Answer";
+}
+
+toggleAnswerButton.addEventListener("click", toggleAnswer);
